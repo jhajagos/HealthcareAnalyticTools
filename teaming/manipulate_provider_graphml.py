6e7d78dadb4864b566df6505e5227c05bca1f0f1
janos <risk.limits@gmail.com>
2014-11-05 12:51:32 -0500

    First pass on filtering nodes and adding command line options

--- teaming/manipulate_provider_graphml.py
+++ teaming/manipulate_provider_graphml.py
@@ -1,29 +1,32 @@
 __author__ = 'janos'
 import csv
 import re
-import sys
 import networkx as nx
+from optparse import OptionParser
+import os
+import sys
 
-def export_edges_to_csv(csv_edge_file_name, provider_graph):
+
+def export_edges_to_csv(csv_edge_file_name, provider_graph_to_export):
     """Export edges to CSV"""
     with open(csv_edge_file_name,"wb") as f:
         csv_edges = csv.writer(f)
-        for node1 in provider_graph.edge:
-            for node2 in provider_graph.edge[node1]:
+        for node1 in provider_graph_to_export.edge:
+            for node2 in provider_graph_to_export.edge[node1]:
                 npi_from = node1
                 npi_to = node2
-                edge_data = provider_graph[node1][node2]
+                edge_data = provider_graph_to_export[node1][node2]
                 csv_edges.writerow((npi_from, npi_to, edge_data["weight"]))
 
 
-def export_nodes_to_csv(csv_node_file_name, provider_graph):
+def export_nodes_to_csv(csv_node_file_name, provider_graph_to_export):
     """Export nodes to CSV"""
     with open(csv_node_file_name, "wb") as fw:
         i = 0
         csv_nodes = csv.writer(fw)
-        for node in provider_graph.node:
+        for node in provider_graph_to_export.node:
 
-            node_dict = provider_graph.node[node]
+            node_dict = provider_graph_to_export.node[node]
             if i == 0:
                 header = node_dict.keys()
 
@@ -46,7 +49,8 @@ def export_nodes_to_csv(csv_node_file_name, provider_graph):
     return i
 
 
-def filter_graphml_by_flattened_provider_taxonomies(provider_graph, list_of_taxonomies, field_name="flattened_taxonomy_string", keep_node_that_matches=True, leaf_nodes_only=True):
+def regex_taxonomy(list_of_taxonomies):
+    """Build a regular expression"""
     regex_string = "("
     i = 0
     for taxonomy in list_of_taxonomies:
@@ -57,9 +61,14 @@ def filter_graphml_by_flattened_provider_taxonomies(provider_graph, list_of_taxo
     regex_string += ')'
 
     re_taxonomy_string = re.compile(regex_string)
+    return re_taxonomy_string
 
-    for node_id in provider_graph.nodes():
-        node = provider_graph.node[node_id]
+
+def filter_graphml_by_flattened_provider_taxonomies(provider_graph_to_manipulate, list_of_taxonomies, field_name="flattened_taxonomy_string", keep_node_that_matches=True, leaf_nodes_only=True):
+    """List of taxonomies"""
+    re_taxonomy_string = regex_taxonomy(list_of_taxonomies)
+    for node_id in provider_graph_to_manipulate.nodes():
+        node = provider_graph_to_manipulate.node[node_id]
         if field_name in node:
             flattened_taxonomy_string = node[field_name]
             match_result = re_taxonomy_string.match(flattened_taxonomy_string)
@@ -69,16 +78,16 @@ def filter_graphml_by_flattened_provider_taxonomies(provider_graph, list_of_taxo
                 else:
                     if leaf_nodes_only:
                         if node["node_type"] == "leaf":
-                            provider_graph.remove_node(node_id)
+                            provider_graph_to_manipulate.remove_node(node_id)
                     else:
-                        provider_graph.remove_node(node_id)
+                        provider_graph_to_manipulate.remove_node(node_id)
             else:
                 if keep_node_that_matches:
                     if leaf_nodes_only:
                         if node["node_type"] == "leaf":
-                            provider_graph.remove_node(node_id)
+                            provider_graph_to_manipulate.remove_node(node_id)
                     else:
-                        provider_graph.remove_node(node_id)
+                        provider_graph_to_manipulate.remove_node(node_id)
                 else:
                     pass
         else:
@@ -86,67 +95,117 @@ def filter_graphml_by_flattened_provider_taxonomies(provider_graph, list_of_taxo
                 if leaf_nodes_only:
                     if "node_type" in node:
                         if node["node_type"] == "leaf":
-                            provider_graph.remove_node(node_id)
+                            provider_graph_to_manipulate.remove_node(node_id)
                         else:
                             pass
                     else:
-                        provider_graph.remove_node(node_id)
+                        provider_graph_to_manipulate.remove_node(node_id)
                 else:
-                    provider_graph.remove_node(node_id)
+                    provider_graph_to_manipulate.remove_node(node_id)
+
+    return provider_graph_to_manipulate
 
 
+def add_indicator_taxonomy_field_to_graph(provider_graph_to_manipulate, list_of_taxonomies, indicator_field_name, taxonomy_field_name="flattened_taxonomy_string"):
+    re_taxonomy_string = regex_taxonomy(list_of_taxonomies)
+    for node_id in provider_graph_to_manipulate.nodes():
+        node = provider_graph_to_manipulate.node[node_id]
+        if taxonomy_field_name in node:
+            flattened_taxonomy_string = node[taxonomy_field_name]
+            match_result = re_taxonomy_string.match(flattened_taxonomy_string)
+            if match_result:
+                node[indicator_field_name] = 1
+            else:
+                node[indicator_field_name] = 0
+    return provider_graph_to_manipulate
 
-    return provider_graph
 
+def export_graph_to_csv(base_file_name, export_provider_graph):
 
-def export_graph_to_csv(base_name, provider_graph):
-
-    csv_node_file_name = base_name + ".nodes_db.csv"
-    csv_edge_file_name = base_name + ".edges.csv"
+    csv_node_file_name = base_file_name + ".nodes_db.csv"
+    csv_edge_file_name = base_file_name + ".edges.csv"
 
     print("Writing CSV with nodes")
-    export_nodes_to_csv(csv_node_file_name, provider_graph)
+    export_nodes_to_csv(csv_node_file_name, export_provider_graph)
 
     print("Writing CSV of edges with weights")
-    export_edges_to_csv(csv_edge_file_name, provider_graph)
+    export_edges_to_csv(csv_edge_file_name, export_provider_graph)
 
 
 if __name__ == "__main__":
-    if len(sys.argv) >= 3:
-        graphml_file_name = sys.argv[1]
-        taxonomy_list_string = sys.argv[2]
 
-        taxonomy_list = taxonomy_list_string.split(',')
+    parser = OptionParser()
 
-        print("Reading GraphML file")
+    parser.add_option("-g", "--graphml_file", dest="graphml_file_name", help="")
+    parser.add_option("-t", "--taxonomy_fields", dest="taxonomy_selection_fields", help="")
+    parser.add_option("-x", "--filter", dest="filter_graph", action="store_true", help="")
+    parser.add_option("-a", "--add_an_indicator_field", dest="indicator_field_name", help="")
+    parser.add_option("-e", "--restrict_to_leaf_nodes", dest="restrict_to_leaf_nodes", help="", action="store_false", default=True)
+    parser.add_option("-c", "--include_entire_graph", dest="restrict_to_leaf_nodes", help="", action="store_true")
+    parser.add_option("-d", "--directory", dest="directory", help="", default=None)
+    parser.add_option("-f", "--base_file_name", dest="base_file_name", help="", default=None)
+    parser.add_option("-j", "--json_file_name", dest="json_file_name", help="", default=None)
 
-        provider_graph = nx.read_graphml(graphml_file_name)
+    (options, args) = parser.parse_args()
+
+    if options.taxonomy_selection_fields:
+        taxonomy_list = options.taxonomy_selection_fields.split(',')
+
+    if options.graphml_file_name:
+        graphml_file_name = options.graphml_file_name
+    else:
+        print("Must indicate with -g a graphml file name")
+        sys.exit()
+
+    if options.restrict_to_leaf_nodes:
+        restrict_to_leaf_nodes = True
+    else:
+        restrict_to_leaf_nodes = False
+
+    print("Reading GraphML file")
+    provider_graph = nx.read_graphml(graphml_file_name)
+
+    if options.directory is None:
+        base_directory = os.path.abspath("./")
+    else:
+        base_directory = options.directory
+
+    if options.base_file_name is None:
+        graphml_file_name_only = os.path.split(graphml_file_name)[1]
+        base_name = graphml_file_name_only[: -1 * len(".graphml")]
+        base_name_manipulated = base_name + ".manipulated"
+    else:
+        base_name_manipulated = options.base_file_name
+
+    manipulated_graphml_file_name = base_name_manipulated + ".graphml"
+
+    if options.filter_graph:
 
         number_of_nodes = len(provider_graph.nodes())
-        base_name = graphml_file_name[: -1 * len(".graphml")]
-        base_name_filtered = base_name + ".filtered"
-        filtered_graphml_file_name = base_name_filtered + ".graphml"
-
         print("Filtering graph by taxonomy")
 
-        if len(sys.argv) == 4:
-            leaf_nodes_only = int(sys.argv[3])
-        else:
-            leaf_nodes_only = 1
-
-
-        filtered_graph = filter_graphml_by_flattened_provider_taxonomies(provider_graph, taxonomy_list, leaf_nodes_only=leaf_nodes_only)
-        number_of_nodes_left = len(filtered_graph.nodes())
+        manipulated_graph = filter_graphml_by_flattened_provider_taxonomies(provider_graph, taxonomy_list,
+                                                                            leaf_nodes_only=restrict_to_leaf_nodes)
+        number_of_nodes_left = len(manipulated_graph.nodes())
 
         removed_nodes = number_of_nodes - number_of_nodes_left
         print("Number of nodes in the filtered graph is %s (removed %s nodes)." % (number_of_nodes_left, removed_nodes))
 
+    if options.indicator_field_name:
+        print("Adding indicator field based on taxonomy to the graph")
 
-        print("Exporting graphml to CSV")
-        export_graph_to_csv(base_name_filtered, filtered_graph)
+        if options.indicator_field_name:
+            indicator_field_name = options.indicator_field_name
+        else:
+            indicator_field_name = "taxonomy_field"
 
-        print("Exporting filtered graph")
-        nx.write_graphml(filtered_graph, filtered_graphml_file_name)
+        manipulated_graph = add_indicator_taxonomy_field_to_graph(provider_graph, taxonomy_list, indicator_field_name)
+
+    print("Exporting graphml to CSV")
+    export_graph_to_csv(base_name_manipulated, manipulated_graph)
+
+    print("Exporting filtered graph")
+    nx.write_graphml(manipulated_graph, manipulated_graphml_file_name)
 
 
 
